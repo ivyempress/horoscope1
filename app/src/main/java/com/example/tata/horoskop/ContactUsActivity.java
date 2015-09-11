a887b138e4d6b54518e4aa56e29bdb6aa5d1c50c
Ivana Lukic <ivana.lukic@gmx.com>
2015-09-11 21:31:36 +0200

    STUFF

--- app/src/main/java/com/example/tata/horoskop/ContactUsActivity.java
+++ app/src/main/java/com/example/tata/horoskop/ContactUsActivity.java
@@ -1,9 +1,12 @@
 package com.example.tata.horoskop;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ContactUsActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class ContactUsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +37,23 @@ public class ContactUsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendEmail(View view) {
+        Intent intent = null, chooser = null;
+
+        if(view.getId() == R.id.iv_imageEmail){
+            intent = new Intent(Intent.ACTION_SEND);
+            intent.setData(Uri.parse("mailto:"));
+            String[] to={"ivyempress@ivyempress..com","ivana.lukic@gmx.com"};
+            intent.putExtra(Intent.EXTRA_EMAIL, to);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "From Astro Horoscope app");
+            intent.putExtra(Intent.EXTRA_TEXT, "Hi, how can we improve our app?");
+            intent.setType("message/rfc22");
+            chooser = Intent.createChooser(intent, "Send email");
+            startActivity(chooser);
+
+        }
+
     }
 }
